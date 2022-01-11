43aabc7344851e7739bb20915902d96e67f83076
dinesh <dinesh.py@yahoo.com>
2022-01-11 06:05:59 +0000

    changes made

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    version = '1.0.1';
-    team = 'Tema 3';
+    version = '1.0.4';
+    team = 'Team 3XX';
 }
 module.exports = AppConfig;
